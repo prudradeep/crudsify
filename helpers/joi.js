931f5a8d114c862c43f73546f846404f29fc63d3
PRadeep Kumar <prithviraj.rudraksh@gmail.com>
2024-02-24 23:52:30 +0530

    Added Query & Joi validation helpers

---
+++ helpers/joi.js
@@ -0,0 +1,430 @@
+"use strict";
+
+const _ = require("lodash");
+const Joi = require("joi");
+const configStore = require("../config");
+const queryHelper = require("./query");
+const { getTimestamps, getMetadata } = require("./model");
+const { Sequelize } = require("../models");
+
+let headersValidation;
+if (configStore.get("/authStrategy")) {
+  headersValidation = Joi.object({
+    authorization: Joi.string().required(),
+  }).options({ allowUnknown: true });
+} else {
+  headersValidation = Joi.object().options({ allowUnknown: true });
+}
+
+/**
+ * Checks to see if a field is a valid model property
+ * @param fieldName: The name of the field
+ * @param field: The field being checked
+ * @param model: A model object
+ * @returns {boolean}
+ */
+const isValidField = function (fieldName, field, keys = []) {
+  const timestamps = Object.keys(getTimestamps(Sequelize.DataTypes));
+  const metadata = Object.keys(getMetadata(Sequelize.DataTypes));
+  const invalidFieldNames = [
+    "__t",
+    "__v",
+    configStore.get("/dbPrimaryKey"),
+    ...timestamps,
+    ...metadata
+  ];
+
+  if (keys.indexOf(fieldName) !== -1) return false;
+
+  if (!_.isObject(field)) {
+    return false;
+  }
+
+  if (invalidFieldNames.indexOf(fieldName) > -1) {
+    return false;
+  }
+
+  return true;
+};
+
+/**
+ * Returns a Joi object based on the field type.
+ * @param field: A field from a model.
+ * @returns {*}: A Joi object.
+ */
+const generateJoiModelFromFieldType = function (field) {
+  let model;
+
+  const fieldCopy = _.extend({}, field);
+  let key = null;
+  if (fieldCopy.type) key = fieldCopy.type.key;
+  else key = fieldCopy.key;
+  switch (key) {
+    case "TEXT":
+    case "CITEXT":
+    case "CHAR":
+    case "CIDR":
+    case "BLOB":
+    case "STRING":
+    case "INET":
+    case "UUID":
+    case "MACADDR":
+      if (fieldCopy.regex) {
+        if (!(fieldCopy.regex instanceof RegExp)) {
+          if (fieldCopy.regex.options) {
+            model = Joi.string().regex(
+              fieldCopy.regex.pattern,
+              fieldCopy.regex.options
+            );
+          } else {
+            model = Joi.string().regex(fieldCopy.regex.pattern);
+          }
+        } else {
+          model = Joi.string().regex(fieldCopy.regex);
+        }
+      } else if (fieldCopy.stringType) {
+        switch (fieldCopy.stringType) {
+          case "uri":
+            model = Joi.string().uri();
+            break;
+          case "email":
+            model = Joi.string().email();
+            break;
+          case "token":
+            model = Joi.string().token();
+            break;
+          case "hex":
+            model = Joi.string().hex();
+            break;
+          case "base64":
+            model = Joi.string().base64();
+            break;
+          case "hostname":
+            model = Joi.string().hostname();
+            break;
+          case "lowercase":
+            model = Joi.string().lowercase();
+            break;
+          case "uppercase":
+            model = Joi.string().uppercase();
+            break;
+          case "trim":
+            model = Joi.string().trim();
+            break;
+          case "creditCard":
+            model = Joi.string().creditCard();
+            break;
+          default:
+            model = Joi.string();
+        }
+      } else {
+        model = Joi.string();
+      }
+      break;
+    case "ARRAY":
+      model = Joi.array().items(generateJoiModelFromFieldType(fieldCopy.type));
+      break;
+    case "JSON":
+      model = Joi.array().items(
+        generateJoiModelFromFieldType(fieldCopy.jsonType)
+      );
+      break;
+
+    case "BIGINT":
+    case "DECIMAL":
+    case "DOUBLE PRECISION":
+    case "FLOAT":
+    case "INTEGER":
+    case "MEDIUMINT":
+    case "NUMBER":
+    case "REAL":
+    case "SMALLINT":
+    case "TINYINT":
+      model = Joi.number();
+      break;
+    case "DATE":
+    case "TIME":
+    case "DATEONLY":
+      model = Joi.date();
+      break;
+    case "BOOLEAN":
+      model = Joi.bool();
+      break;
+    case "ENUM":
+      model = Joi.string().valid(...fieldCopy.values);
+      break;
+    default:
+      model = Joi.any();
+      break;
+  }
+
+  if (fieldCopy.allowNull) {
+    model = model.allow(null);
+  }
+
+  if (fieldCopy.description) {
+    model = model.description(fieldCopy.description);
+  } else if (fieldCopy.stringType) {
+    model = model.description(fieldCopy.stringType);
+  }
+
+  return model;
+};
+
+/**
+ * Generates a Joi object that validates a request query for the list function
+ * @param model: A model object.
+ * @returns {*}: A Joi object
+ */
+const generateJoiListQueryModel = (model) => {
+  let queryModel = {
+    $skip: Joi.number()
+      .integer()
+      .min(0)
+      .optional()
+      .description(
+        "The number of records to skip in the database. This is typically used in pagination."
+      ),
+    $page: Joi.number()
+      .integer()
+      .min(0)
+      .optional()
+      .description(
+        "The number of records to skip based on the $limit parameter. This is typically used in pagination."
+      ),
+    $limit: Joi.number()
+      .integer()
+      .min(0)
+      .optional()
+      .description(
+        "The maximum number of records to return. This is typically used in pagination."
+      ),
+  };
+
+  const queryableFields = queryHelper.getQueryableFields(model);
+
+  const readableFields = queryHelper.getReadableFields(model);
+
+  const sortableFields = queryHelper.getSortableFields(model);
+
+  const operations = [
+    "=",
+    "<",
+    "<=",
+    ">",
+    ">=",
+    "between",
+    "notBetween",
+    "in",
+    "notIn",
+    "like",
+    "notLike",
+  ];
+
+  if (queryableFields && readableFields) {
+    queryModel.$select = Joi.alternatives().try(
+      Joi.array()
+        .items(Joi.string().valid(...readableFields))
+        .description(
+          "A list of basic fields to be included in each resource. Valid values include: " +
+            readableFields.toString().replace(/,/g, ", ")
+        ),
+      Joi.string().valid(...readableFields)
+    );
+    queryModel.$sort = Joi.alternatives().try(
+      Joi.array()
+        .items(Joi.string().valid(...sortableFields))
+        .description(
+          "A set of fields to sort by. Including field name indicates it should be sorted ascending, while prepending " +
+            "'-' indicates descending. The default sort direction is 'ascending' (lowest value to highest value). Listing multiple" +
+            "fields prioritizes the sort starting with the first field listed. Valid values include: " +
+            sortableFields.toString().replace(/,/g, ", ")
+        ),
+      Joi.string().valid(...sortableFields)
+    );
+    queryModel.$exclude = Joi.alternatives().try(
+      Joi.array()
+        .items(Joi.number())
+        .description("A list of Ids to exclude in the result."),
+      Joi.number()
+    );
+    queryModel.$count = Joi.boolean().description(
+      "If set to true, only a count of the query results will be returned."
+    );
+
+    _.each(queryableFields, function (fieldName) {
+      const joiModel = generateJoiModelFromFieldType(
+        model.rawAttributes[fieldName]
+      );
+      queryModel[`${fieldName}Oper`] = Joi.string()
+        .valid(...operations)
+        .description(`Allowed values are ${operations.join(", ")}`);
+      queryModel[fieldName] = Joi.alternatives().try(
+        Joi.array()
+          .items(joiModel)
+          .description("Match values for the " + fieldName + " property."),
+        joiModel
+      );
+    });
+  }
+
+  const associations = model.associations ? model.associations : null;
+  if (associations) {
+    let embeds = [];
+    for (const assoc of Object.values(model.associations)) {
+      const { target } = assoc;
+      embeds.push(target.name);
+    }
+    queryModel.$embed = Joi.alternatives().try(
+      Joi.array()
+        .items(Joi.string())
+        .description(
+          "A set of complex object properties to populate. Valid first level values include " +
+            Object.values(embeds).toString().replace(/,/g, ", ")
+        ),
+      Joi.string()
+    );
+  }
+
+  queryModel = Joi.object(queryModel);
+
+  if (!configStore.get("/enableQueryValidation")) {
+    queryModel = queryModel.unknown();
+  }
+
+  return queryModel;
+};
+
+/**
+ * Generates a Joi object that validates a request query for the find function
+ * @param model: A model object.
+ * @returns {*}: A Joi object
+ */
+const generateJoiFindQueryModel = (model) => {
+  let queryModel = {};
+
+  const readableFields = queryHelper.getReadableFields(model);
+
+  if (readableFields) {
+    queryModel.$select = Joi.alternatives().try(
+      Joi.array()
+        .items(Joi.string().valid(...readableFields))
+        .description(
+          "A list of basic fields to be included in each resource. Valid values include: " +
+            readableFields.toString().replace(/,/g, ", ")
+        ),
+      Joi.string().valid(...readableFields)
+    );
+  }
+
+  const associations = model.associations ? model.associations : null;
+  if (associations) {
+    let embeds = [];
+    for (const assoc of Object.values(model.associations)) {
+      const { target } = assoc;
+      embeds.push(target.name);
+    }
+    queryModel.$embed = Joi.alternatives().try(
+      Joi.array()
+        .items(Joi.string())
+        .description(
+          "A set of complex object properties to populate. Valid first level values include " +
+            Object.values(embeds).toString().replace(/,/g, ", ")
+        ),
+      Joi.string()
+    );
+  }
+
+  queryModel = Joi.object(queryModel);
+
+  if (!configStore.get("/enableQueryValidation")) {
+    queryModel = queryModel.unknown();
+  }
+
+  return queryModel;
+};
+
+/**
+ * Generates a Joi object that validates a request payload for creating a document
+ * @param model: A model object.
+ * @returns {*}: A Joi object
+ */
+const generateJoiCreateModel = (model, assoc = false, keys = []) => {
+  const createModelBase = {};
+
+  const fields = model.rawAttributes;
+
+  for (const fieldName in fields) {
+    const field = fields[fieldName];
+    if (isValidField(fieldName, field, keys)) {
+      // EXPL: use the field createModel if one is defined
+      if (field.createModel) {
+        createModelBase[fieldName] = field.createModel;
+      } else if (field.allowOnCreate !== false) {
+        let attributeCreateModel = generateJoiModelFromFieldType(field);
+
+        if (field.allowNull === false) {
+          attributeCreateModel = attributeCreateModel.required();
+        }
+
+        createModelBase[fieldName] = attributeCreateModel;
+      }
+    }
+  }
+
+  if (assoc) {
+    const createModel = Joi.array()
+      .items(createModelBase)
+      .label(model.name + "CreateModel");
+    return createModel;
+  }
+
+  const createModel = Joi.object(createModelBase).label(
+    model.name + "CreateModel"
+  );
+
+  return createModel;
+};
+
+/**
+ * Generates a Joi object that validates a query request payload for updating a document
+ * @param model: A model object.
+ * @returns {*}: A Joi object
+ */
+const generateJoiUpdateModel = (model, keys = []) => {
+  const updateModelBase = {};
+
+  const fields = model.rawAttributes;
+
+  for (const fieldName in fields) {
+    const field = fields[fieldName];
+
+    if (isValidField(fieldName, field, keys)) {
+      if (field.updateModel) {
+        updateModelBase[fieldName] = field.updateModel;
+      } else if (field.allowOnUpdate !== false) {
+        let attributeUpdateModel = generateJoiModelFromFieldType(field);
+
+        if (field.requireOnUpdate === true) {
+          attributeUpdateModel = attributeUpdateModel.required();
+        }
+
+        updateModelBase[fieldName] = attributeUpdateModel;
+      }
+    }
+  }
+
+  const updateModel = Joi.object(updateModelBase).label(
+    model.name + "UpdateModel"
+  );
+
+  return updateModel;
+};
+
+module.exports = {
+  generateJoiListQueryModel,
+  generateJoiFindQueryModel,
+  generateJoiCreateModel,
+  generateJoiUpdateModel,
+  headersValidation,
+};
---
+++ helpers/query.js
@@ -0,0 +1,309 @@
+"use strict";
+
+const _ = require("lodash");
+const configStore = require("../config");
+const { Op, fn, col } = require("sequelize");
+
+module.exports = {
+  /**
+   * Handle pagination for the query if needed.
+   * @param query: The incoming request query.
+   * @returns {*}: The updated query.
+   */
+  paginate: function (query) {
+    let paginate = {};
+    if (query.$page) {
+      paginate = {
+        ...paginate,
+        ...this.setPage(query),
+      };
+    } else {
+      paginate = {
+        ...paginate,
+        ...this.setSkip(query),
+      };
+    }
+
+    paginate = {
+      ...paginate,
+      ...this.setLimit(query),
+    };
+
+    return paginate;
+  },
+
+  /**
+   * Set the skip amount for the query. Typically used for paging.
+   * @param query: The incoming request query.
+   * @returns {*}: The updated query.
+   */
+  setSkip: function (query) {
+    if (query.$skip) {
+      return { offset: query.$skip };
+    }
+    return {};
+  },
+
+  /**
+   * Set the page for the query. Typically used for paging.
+   * @param query: The incoming request query.
+   * @returns {*}: The updated query.
+   */
+  setPage: function (query) {
+    return {
+      offset:
+        (parseInt(query.$page) - 1) *
+        (query.$limit ? parseInt(query.$limit) : configStore.get("/limit")),
+    };
+  },
+
+  /**
+   * Set the limit amount for the query. Typically used for paging.
+   * @param query: The incoming request query.
+   * @returns {*}: The updated query.
+   */
+  setLimit: function (query) {
+    if (query.$limit) {
+      return { limit: parseInt(query.$limit) };
+    }
+    return { limit: configStore.get("/limit") };
+  },
+
+  /**
+   * Set the sort priority for the mongoose query.
+   * @param query: The incoming request query.
+   * @returns {*}: The updated mongoose query.
+   */
+  setSort: function (query) {
+    if (query.$sort) {
+      if (Array.isArray(query.$sort)) {
+        return query.$sort.map((val) => {
+          if (val.indexOf("-") == 0) {
+            return [val.substr(1), "DESC"];
+          }
+          return [val, "ASC"];
+        });
+      } else {
+        if (query.$sort.indexOf("-") == 0) {
+          return [[query.$sort.substr(1), "DESC"]];
+        }
+        return [[query.$sort, "ASC"]];
+      }
+    }
+    return [];
+  },
+
+  /**
+   * Get a list of fields that can be returned as part of a query result.
+   * @param model: A model object.
+   * @returns {Array}: A list of fields.
+   */
+  getReadableFields: function (model) {
+    const readableFields = [];
+
+    const fields = model.rawAttributes;
+    for (const fieldName in fields) {
+      const field = fields[fieldName];
+      if (!field.exclude && fieldName !== "__v") {
+        readableFields.push(fieldName);
+      }
+    }
+
+    return readableFields;
+  },
+
+  /**
+   * Get a list of valid query sort inputs.
+   * @param model: A model object.
+   * @returns {Array}: A list of fields.
+   */
+  getSortableFields: function (model) {
+    const sortableFields = this.getReadableFields(model);
+
+    for (let i = sortableFields.length - 1; i >= 0; i--) {
+      const descendingField = "-" + sortableFields[i];
+      sortableFields.splice(i, 0, descendingField);
+    }
+
+    return sortableFields;
+  },
+
+  /**
+   * Get a list of fields that can be queried against.
+   * @param model: A model object.
+   * @returns {Array}: A list of fields.
+   */
+  getQueryableFields: function (model) {
+    const queryableFields = [];
+
+    const fields = model.rawAttributes;
+
+    const fieldNames = Object.keys(fields);
+    for (let i = 0; i < fieldNames.length; i++) {
+      const fieldName = fieldNames[i];
+      if (fields[fieldName] && fieldName !== "__v" && fieldName !== "__t") {
+        const field = fields[fieldName];
+        if (field.queryable !== false && !field.exclude) {
+          queryableFields.push(fieldName);
+        }
+      }
+    }
+
+    return queryableFields;
+  },
+
+  getFieldsType: function (model) {
+    const _fields = model.rawAttributes;
+    const fields = {};
+
+    for (const fieldName in _fields) {
+      const field = _fields[fieldName];
+      fields[fieldName] = field.type.key;
+    }
+    return fields;
+  },
+
+  createWhereCondition: function (query, model) {
+    let where = [];
+    const fields = this.getFieldsType(model);
+    const regex = /Oper$/gm;
+    let Oper = false;
+
+    Object.keys(query).forEach((val) => {
+      if (regex.test(val)) {
+        Oper = { oper: query[val], key: val.replace("Oper", "") };
+        return true;
+      }
+      if (val.indexOf("$") === -1) {
+        if (_.isArray(query[val]))
+          query[val] = query[val].map((v) => {
+            if (typeof v === "string")
+              return v.toLowerCase().replace("%20", " ");
+            return v;
+          });
+        else if (typeof query[val] === "string")
+          query[val].toLowerCase().replace("%20", " ");
+        if (fields[val] === "JSON") {
+          where.push(
+            fn("JSON_SEARCH", fn("LOWER", col(val)), "one", query[val])
+          );
+        } else if (_.isArray(query[val])) {
+          query[val] = query[val].map((v) => {
+            if (v === "true") return true;
+            if (v === "false") return false;
+            return v;
+          });
+          if (Oper && Oper.key === val) {
+            switch (Oper.oper) {
+              case "between":
+                where.push({ [val]: { [Op.between]: query[val] } });
+                break;
+              case "notBetween":
+                where.push({ [val]: { [Op.notBetween]: query[val] } });
+                break;
+              case "in":
+                where.push({ [val]: { [Op.in]: query[val] } });
+                break;
+              case "notIn":
+                where.push({ [val]: { [Op.notIn]: query[val] } });
+                break;
+            }
+            Oper = false;
+          } else {
+            where.push({ [val]: { [Op.in]: query[val] } });
+          }
+        } else {
+          if (query[val] === "true") query[val] = true;
+          if (query[val] === "false") query[val] = false;
+
+          if (Oper && Oper.key === val) {
+            switch (Oper.oper) {
+              case "=":
+                where.push({ [val]: { [Op.eq]: query[val] } });
+                break;
+              case "<":
+                where.push({ [val]: { [Op.lt]: query[val] } });
+                break;
+              case "<=":
+                where.push({ [val]: { [Op.lte]: query[val] } });
+                break;
+              case ">":
+                where.push({ [val]: { [Op.gt]: query[val] } });
+                break;
+              case ">=":
+                where.push({ [val]: { [Op.gte]: query[val] } });
+                break;
+              case "between":
+                where.push({ [val]: { [Op.between]: query[val] } });
+                break;
+              case "notBetween":
+                where.push({ [val]: { [Op.notBetween]: query[val] } });
+                break;
+              case "in":
+                where.push({ [val]: { [Op.in]: query[val] } });
+                break;
+              case "notIn":
+                where.push({ [val]: { [Op.notIn]: query[val] } });
+                break;
+              case "like":
+                where.push({ [val]: { [Op.like]: query[val] } });
+                break;
+              case "notLike":
+                where.push({ [val]: { [Op.notLike]: query[val] } });
+                break;
+            }
+            Oper = false;
+          } else {
+            where.push({ [val]: query[val] });
+          }
+        }
+      } else if (val === "$exclude") {
+        if (_.isArray(query[val])) {
+          where.push({
+            [configStore.get("/dbPrimaryKey")]: { [Op.notIn]: query[val] },
+          });
+        } else {
+          where.push({
+            [configStore.get("/dbPrimaryKey")]: { [Op.ne]: query[val] },
+          });
+        }
+      }
+    });
+    return { where: { [Op.and]: where } };
+  },
+
+  getEmbeds: function (DB, embed) {
+    let embeds = [];
+    if (_.isArray(embed)) {
+      embeds = embed.map((val) => {
+        if (val.indexOf(".") !== -1) {
+          const em = val.split(".").reduceRight((init, c) => {
+            const init_ = {
+              model: DB[c],
+            };
+            if (!_.isArray(init)) {
+              init_.include = [init];
+            }
+            return init_;
+          }, []);
+          return em;
+        }
+        return { model: DB[val] };
+      });
+    } else {
+      if (embed.indexOf(".") !== -1) {
+        embeds = embed.split(".").reduceRight((init, val) => {
+          const init_ = {
+            model: DB[val],
+          };
+          if (!_.isArray(init)) {
+            init_.include = [init];
+          }
+          return init_;
+        }, []);
+        embeds = [embeds];
+      } else embeds = [{ model: DB[embed] }];
+    }
+    return embeds;
+  },
+};
