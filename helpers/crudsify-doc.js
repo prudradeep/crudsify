cf280b3fa16b1d3418a019edca5d790d77346553
Pradeep <prithviraj.rudraksh@gmail.com>
2026-08-17 18:37:11 +0530

    Document search fixes

--- helpers/crudsify-doc.js
+++ helpers/crudsify-doc.js
@@ -6,7 +6,20 @@ const path = require("path");
 const crudsifyDocRouter = express.Router();
 const docPath = path.join(__dirname, "../docs/crudsifydoc.html");
 
+const docContentSecurityPolicy = [
+  "default-src 'self'",
+  "base-uri 'self'",
+  "font-src 'self' data:",
+  "form-action 'self'",
+  "frame-ancestors 'self'",
+  "img-src 'self' data:",
+  "object-src 'none'",
+  "script-src 'self' 'unsafe-inline'",
+  "style-src 'self' 'unsafe-inline'",
+].join("; ");
+
 crudsifyDocRouter.get("/crudsifydoc", (req, res) => {
+  res.set("Content-Security-Policy", docContentSecurityPolicy);
   res.sendFile(docPath);
 });
 
